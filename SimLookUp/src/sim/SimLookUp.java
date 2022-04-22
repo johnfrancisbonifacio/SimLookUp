1a637efabd3a1b482ad6bdb909065b193a563f5c
John Francis Bonifacio <40020200+johnfrancisbonifacio@users.noreply.github.com>
2022-04-22 21:51:16 +0800

    It's working now. Thanks to stackoverflow
    https://stackoverflow.com/questions/68790276/pushing-from-eclipse-to-my-github-repository-via-https-stopped-working-git-rec

--- SimLookUp/src/sim/SimLookUp.java
+++ SimLookUp/src/sim/SimLookUp.java
@@ -91,6 +91,7 @@ public class SimLookUp {
 		System.out.print("Enter:  ");
 		//From Github: Sample comment. Try to pull
 		//From local: The Pull request working. Let me know if you receive this message by adding 'Confirmed' comment after this line.
+		//From Github: Confirmed.
 		try {
 			String val = in.next();
 			if (val == "" || val.equals("")) {
